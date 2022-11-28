851676b6eb377994f1cda4d9fa281de3f0066832
Matt Kuehr <matt914811@gmail.com>
2022-11-28 15:54:59 -0600

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/**
+Short program to print the letter J out of characters.
+Project 1
+@MattKuehr
+@August25th2022
+*/
+public class JLetter
+{
+   /** 
+   Main method to print the letter J comprised of 
+   other letters line by line.
+   @param args Command line arguments - not used.
+   */
+   public static void main(String[]args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,28 @@
+/**
+Prints my life goals, short, medium and long term.
+Project 1
+@MattKuehr
+@August25th2022 
+*/
+public class MyLifeGoals
+{
+   /**
+   Main Method to execute printing of my name, blank line, 
+   then goals each on a separate line.
+   @param args Command line arguments - not used. 
+   */
+   public static void main(String[]args)
+   {
+      System.out.println("Matt Kuehr");
+      System.out.println();
+      System.out.println("My short term goal is to do well in this class."
+               + "I have quite little experience in programming so"
+               + "I am excited to learn and improve in this new field.");
+      System.out.println("My medium term goal is to graduate college."
+               + "I think graduating college will set me up well"
+               + "to enter a competitive and reliable field.");
+      System.out.println("My long term goal is to use my skills to help other"
+               + "people. I gain the most enjoyment out of"
+               + "seeing my work better the lives of others.");
+   }
+}
